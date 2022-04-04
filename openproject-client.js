e8647f03cff3cf4a73cfbfa25da0b193ff9e58cc
Dominic <dominic.schweizer@bfh.ch>
2022-04-04 13:48:30 +0200

    openproject connection works

--- openproject-client.js
+++ openproject-client.js
@@ -1,6 +1,16 @@
+require("dotenv").config();
 const axios = require("axios").default;
-
+console.log(process.env);
 axios
-  .get("google.ch")
-  .then((response) => console.log)
-  .catch((error) => console.log);
+  .get("https://openproject.fdn-tools.inf.unibe.ch/api/v3/work_packages", {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Basic ${btoa(`apikey:${process.env.OPENPROJECTAPI}`)}`,
+    },
+  })
+  .then((response) => {
+    console.log("Got a response"), console.log(response.data);
+  })
+  .catch((error) => {
+    console.log("Got an error"), console.log(error);
+  });
